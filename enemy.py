d61274423b31e89d6d794d69bf5a0710503c079f
CrefconGG <miss.sunisa1978@gmail.com>
2023-10-14 15:29:10 +0700

    Fix Enemy speed

--- enemy.py
+++ enemy.py
@@ -8,16 +8,18 @@ class Enemy(pg.sprite.Sprite):
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = pos
+        self.speed = 0.5
+        self.animation_speed = 1
 
     def update(self):
         self.animate()
         self.move()
 
     def animate(self):
-        self.index += 1
+        self.index += self.animation_speed
         if self.index >= len(self.images):
             self.index = 0
         self.image = self.images[self.index]
     
     def move(self):
-        self.rect.x += 0.5
+        self.rect.x += self.speed
